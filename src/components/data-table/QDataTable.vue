8619d4809115d2c2b529f2bcf4d876df4168d7c1
Francesco Saverio Giudice <fsgiudice@conxulta.it>
2020-10-03 16:50:13 +0200

    fixed error on entries value when data empty

--- src/components/data-table/QDataTable.vue
+++ src/components/data-table/QDataTable.vue
@@ -308,6 +308,7 @@ export default {
       let data = this.internalData || []
 
       if (!data.length) {
+        this.pagination.entries = 0
         return []
       }
 
@@ -349,6 +350,7 @@ export default {
         this.sort(rows)
       }
 
+      // console.log('this.pagination.entries, rows.length', this.pagination.entries, rows.length)
       this.pagination.entries = rows.length
       if (this.pagination.rowsPerPage > 0) {
         rows = this.paginate(rows)
@@ -410,7 +412,8 @@ export default {
     displayFormat () {
       let f = this.config.format || '%T'
       f = f.replace('%T', this.config.title)
-      f = f.replace('%N', this.rows.length)
+      // f = f.replace('%N', this.rows.length)
+      f = f.replace('%N', this.rows.length > 0 ? this.pagination.entries : 0)
       if (this.config.labels && this.config.labels.textNumber) {
         f = f.replace('%text_number', this.rows.length === 1 ? this.config.labels.textNumber.singular || '' : this.config.labels.textNumber.plural || '')
       }
